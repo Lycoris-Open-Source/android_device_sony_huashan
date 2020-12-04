0e9b77f9d609d37fd1aa18837a730b6066a710b6
115ek <e.blosz@hotmail.de>
2020-12-04 21:12:46 +0100

    huashan: boot: Move files from /sbin to /bin
    * Android 11 globally removed /sbin
      https://github.com/LineageOS/android_system_core/commit/2f74a26a2dbe092d63f1e46e6471369d4a19adce#diff-ac3a2ef33fb4b89653911e274441a0a00da7c30371c1a36af05f22d054ea4cce
    
    Change-Id: I148cf3ee7e3bef809eb4285ad9b3d9de4189d8fb

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -90,7 +90,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
 	$(hide) cp -a $(TARGET_RAMDISK_OUT) $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/bin
 
-	$(hide) cp $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
+	$(hide) mv $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
 	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/bin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/bin/
 	$(hide) cp $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(PRODUCT_OUT)/combinedroot/bin/toybox_init
